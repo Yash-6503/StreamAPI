2802e14038f77196a4f23f4bbfea6698b0e1fb6a
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2025-12-08 15:01:55 +0530

    Var Keyword Example

---
+++ Examples/Testing/VarKeyword.java
@@ -0,0 +1,19 @@
+package com.classes;
+
+public class VarKeyword {
+    public static void main(String[] args) {
+        var x = 12;
+        System.out.println(x);
+
+        x = 90;
+        System.out.println(x);
+
+        // x = "NIT"; //Invalid
+
+        var name = "Yash";
+        System.out.println(name);
+
+        // name = 20;
+
+    }
+}
