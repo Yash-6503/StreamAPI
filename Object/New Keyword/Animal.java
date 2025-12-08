72a73e082b9ff1c491242963b6d129cae8ceaf64
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2025-12-08 17:01:32 +0530

    Creating Object by new keyword

---
+++ Object/New Keyword/Animal.java
@@ -0,0 +1,10 @@
+// Program to create object using new keyword
+
+public class Animal {
+    public static void main(String[] args) {
+        
+        //Creating object using new keyword
+        Dog dog = new Dog();
+        dog.sound();
+    }   
+}
