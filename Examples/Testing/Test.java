ddbdddc0fb10e9f8682929846d8412ecc4980c46
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2025-12-08 15:03:05 +0530

    Test Example

---
+++ Examples/Testing/Test.java
@@ -0,0 +1,14 @@
+package com.classes;
+
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+       
+        System.out.println(5 & 6); // 4
+        System.out.println(5 | 6); // 7
+        System.out.println(5 ^ 6); // 3
+
+    }
+
+}
